2ed93b11ca24132469a4cbb0fabb6e700aea3a17
DanhVinhLe <132262572+DanhVinhLe@users.noreply.github.com>
2025-04-14 13:37:01 +0700

    fix decoder

--- stable_diffusion/decoder.py
+++ stable_diffusion/decoder.py
@@ -22,7 +22,7 @@ class VAE_AttentionBlock(nn.Module):
         return x
 
 class VAE_ResidualBlock(nn.Module):
-    def _init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
         self.groupnorm_1 = nn.GroupNorm(32, in_channels)
         self.conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
