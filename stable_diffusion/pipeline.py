7fb3bc35fd0c415a6fdb21bec1cd737767164a2d
DanhVinhLe <132262572+DanhVinhLe@users.noreply.github.com>
2025-04-14 15:33:40 +0700

    fix pipeline

--- stable_diffusion/pipeline.py
+++ stable_diffusion/pipeline.py
@@ -13,6 +13,7 @@ def rescale(x, old_range, new_range, clamp = False):
     new_min, new_max = new_range
     x -= old_min
     x *= (new_max - new_min) / (old_max - old_min)
+    x += new_min
     if clamp:
         x = x.clamp(new_min, new_max)
     return x
